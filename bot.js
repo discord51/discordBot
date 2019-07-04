a731fe6a16f8500f781592f2b7366ac670596a67
discord51 <52514826+discord51@users.noreply.github.com>
2019-07-04 03:11:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,15 @@
-
+client.on('message', message => {
+  if (message.content === ('%bot')) {
+  message.channel.send({
+      embed: new Discord.RichEmbed()
+          .setAuthor(client.user.username,client.user.avatarURL)
+          .setThumbnail(client.user.avatarURL)
+          .setColor('RANDOM')
+          .addField('**Servers**?? :', [client.guilds.size], true)
+          .addField('**Users**?? :' ,`[ ${client.users.size} ]` , true)
+          .addField('**Support**?? :' , `[سيرفر السبورت]` , true)
+          .addField('**Developers**?? :' , `[<@الأيدي حقك>]` , true)
+          .setFooter(message.author.username, message.author.avatarURL)
+  })
+}
+});
